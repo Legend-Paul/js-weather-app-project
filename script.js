56f97c6116687b4d56c764ebcab7fcdf9b12cfa7
Legend Paul <qamunyamaina950@gmail.com>
2025-04-28 12:37:57 +0300

    created a function to get the city searched by the user

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+let cityInput = document.querySelector(".city-input");
+let cityName = null;
+function getCityName() {
+    document.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            console.log("Button clicked");
 
+            console.log(cityName);
+            if (cityInput.value) {
+                cityName = cityInput.value;
+                cityInput.value = "";
+            }
+        }
+    });
+}
+getCityName();
