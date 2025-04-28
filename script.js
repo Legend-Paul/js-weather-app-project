04369fe01a8582e64433a583062bdd427bbb8e2c
Legend Paul <qamunyamaina950@gmail.com>
2025-04-28 16:09:29 +0300

    created a function to fetch information of the current weather and returns a json

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let cityInput = document.querySelector(".city-input");
-let cityName = null;
+let currentCityTemp = document.querySelector(".city-temp");
+let currentCityName = document.querySelector(".city-name");
+let currentMaxTemp = document.querySelector(".max-temp");
+let currentMinTemp = document.querySelector(".min-temp");
+let currentDay = document.querySelector(".date");
+let day = new Date();
+
 function getCityName() {
     document.addEventListener("keypress", function (event) {
         if (event.key === "Enter") {
@@ -14,3 +20,15 @@ function getCityName() {
     });
 }
 getCityName();
+
+async function getCityInfo() {
+    try {
+        let response = await fetch(
+            "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Nyeri%2C%20Kenya?unitGroup=us&key=ZWPL3H2TS58FU9V7KU84YNYK3&contentType=json"
+        );
+        let data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
