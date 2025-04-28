1316d8562a1dfe30351209f450088de728b44f25
Legend Paul <qamunyamaina950@gmail.com>
2025-04-28 16:11:49 +0300

    Added function to display the city weather information on that day

--- script.js
+++ script.js
@@ -5,6 +5,34 @@ let currentMaxTemp = document.querySelector(".max-temp");
 let currentMinTemp = document.querySelector(".min-temp");
 let currentDay = document.querySelector(".date");
 let day = new Date();
+let daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+let monthsOfYear = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+let todayDay = daysOfWeek[day.getDay()];
+let todayMonth = monthsOfYear[day.getMonth()];
+let todayDate = day.getDate();
+let todayYear = day.getFullYear();
 
 function getCityName() {
     document.addEventListener("keypress", function (event) {
@@ -41,3 +69,33 @@ function changeFarhenheitToCelsius(fahrenheit) {
     let celsius = (fahrenheit - 32) * (5 / 9);
     return celsius.toFixed(2);
 }
+
+function displayCityInfo() {
+    getCityInfo()
+        .then((data) => {
+            let cityName = data.resolvedAddress;
+            let cityTemp = changeFarhenheitToCelsius(
+                data.currentConditions.temp
+            );
+            let maxTemp = changeFarhenheitToCelsius(data.days[0].tempmax);
+            let minTemp = changeFarhenheitToCelsius(data.days[0].tempmin);
+            let cityWeather = data.currentConditions.conditions;
+            let weatherIcon = data.currentConditions.icon;
+
+            currentCityName.innerHTML = cityName;
+            currentCityTemp.innerHTML = `${cityTemp}°C`;
+            currentMinTemp.innerHTML = `${minTemp}°C`;
+            currentMaxTemp.innerHTML = `${maxTemp}°C`;
+            currentDay.innerHTML = `${todayDay}, ${todayMonth} ${todayDate}, ${todayYear}`;
+            document.querySelector(".weather-description").innerHTML =
+                cityWeather;
+
+            document.querySelector(
+                ".weather-icon"
+            ).src = `imgs/${weatherIcon}.png`;
+        })
+        .catch((error) => {
+            console.error("Error displaying city info:", error);
+        });
+}
+displayCityInfo();
